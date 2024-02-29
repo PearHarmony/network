3976d9bf361f91e647a3c1fcdacb0837de84d821
Exstreme_Noob <116436587+Exstreme-Noob@users.noreply.github.com>
2024-02-29 11:23:17 +0100

    daniel ist dumm

--- src/org/pearharmony/Decoder.java
+++ src/org/pearharmony/Decoder.java
@@ -8,17 +8,32 @@ import java.nio.file.StandardOpenOption;
 import java.io.*;
 
 public class Decoder {
+    public byte[] cleanData(byte[] _data)
+    {
+        byte[] clean = new byte[_data.length-1];
+        for(int i = 1;i<clean.length;i++)
+        {
+            _data[i]=clean[i-1];
+        }
+        return clean;
+    }
+    public byte getType(byte[] _data)
+    {
+        return _data[0];
+    }
 
     public String textDecode(byte[] _data) {
         return new String(_data, StandardCharsets.UTF_8);
     }
 
-    public void pictureDecode(byte[] _data, String _path) {
-        Path path = Paths.get(_path, System.currentTimeMillis() + ".png");
+    public Path pictureDecode(byte[] _data, String _path) {
+        Path path = Paths.get(_path,System.currentTimeMillis()+ ".png");
         try {
             Files.write(path, _data, StandardOpenOption.CREATE);
+            return path;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
 }
